07783696b2ffb7d67fadded3550888a962e24eaf
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-12-04 16:45:32 +0100

    create colors file and replace lucide-icons with expo-vector icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,21 @@
 import { Tabs } from "expo-router";
-import { Grid3X3, Headphones, Home, Settings as SettingsIcon } from "lucide-react-native";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import Colors from "@/constants/colors";
-import { View } from "react-native";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {    // doesn't support native wind css styles
+        tabBarStyle: {
           backgroundColor: Colors.palette.surface,
           borderTopColor: "transparent",
           height: 74,
         },
         tabBarActiveTintColor: Colors.light.tabIconSelected,
         tabBarInactiveTintColor: Colors.light.tabIconDefault,
-        tabBarLabelStyle: {  // doesn't support native wind css styles
+        tabBarLabelStyle: {
           fontSize: 12,
           fontWeight: "600",
         },
@@ -26,28 +25,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,  // doesn't support native wind css styles
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="categories"
         options={{
           title: "Categories",
-          tabBarIcon: ({ color, size }) => <Grid3X3 color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="grid" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="player"
         options={{
           title: "Player",
-          tabBarIcon: ({ color, size }) => <Headphones color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="headset" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
-          tabBarIcon: ({ color, size }) => <SettingsIcon color={color} size={size} />,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
