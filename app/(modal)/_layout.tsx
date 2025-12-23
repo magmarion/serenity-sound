cd03cb34094321bc2e3698f5d35c19af8b4ee993
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-12-23 18:12:07 +0100

    add modal layout and make player modal transparent and overlay current screen

---
+++ app/(modal)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function ModalLayout() {
+    return (
+        <Stack
+            screenOptions={{
+                presentation: "transparentModal",
+                headerShown: false,
+                contentStyle: {
+                    backgroundColor: "transparent",
+                },
+            }}
+        />
+    );
+}
