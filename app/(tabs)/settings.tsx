20b862e0b15e50a28ae2493b904a28e0cc7f46bf
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-12-25 18:56:17 +0100

    remove unused import

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,5 @@
 // app/(tabs)/settings.tsx
 import { FontAwesome6, Ionicons } from "@expo/vector-icons";
-import * as Haptics from "expo-haptics";
 import { LinearGradient } from "expo-linear-gradient";
 import { Stack, useRouter } from "expo-router";
 import React, { memo, useCallback, useMemo, useState } from "react";
